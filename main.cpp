e613c6fdb6da2035b7317b35334bcf548b5b06f0
rhodeon <ruonabraks@gmail.com>
2020-06-01 00:22:06 +0100

    Implemented audio and cleaned up code structure

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
 
 #include <chrono>
 #include <thread>
@@ -25,7 +26,20 @@ enum KEY_PRESSES {
     KEY_PRESS_F,
 };
 
+void init_sdl() {
+    /* 
+     * Initializes SDL subsystems and mixer
+     */
+
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO); 
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096);
+}
+
 void init_window(SDL_Window*& window, SDL_Surface*& surface, int width, int height) {
+    /* 
+     * Initializes main window
+     */
+
     window = SDL_CreateWindow("CHIP-Oct", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
     surface = SDL_GetWindowSurface(window);
 }
@@ -35,22 +49,31 @@ void quit(SDL_Window*& window) {
     window = NULL;
 }
 
-void draw_graphics(chip8& game, SDL_Window*& window, SDL_Surface*& base_surface, int width, int height) {
-    SDL_Rect pixel;
-    pixel.w = 10;
-    pixel.h = 10;
-    std::array<unsigned char, 64 * 32> screen = game.display;
+void draw_graphics(chip8& game, SDL_Window*& window, SDL_Surface*& base_surface) {
+    /* 
+     * Copies the pixels in the display buffer and displays them on the corresponding positions on the screen
+    */
 
-    for (int row = 0; row < height; row += 10) {
-        for (int column = 0; column < width; column += 10) {
+    // width and height of CHIP-8 platform
+    const int c8_width = 64;
+    const int c8_height = 32;
+    
+    SDL_Rect pixel;
+    pixel.w = base_surface->w / c8_width;
+    pixel.h = base_surface->h / c8_height;
+    std::array<unsigned char, c8_width * c8_height> screen = game.display;
+
+    // Draw each pixel if corresponding value in display buffer is 1
+    for (int row = 0; row < base_surface->h; row += pixel.h) {
+        for (int column = 0; column < base_surface->w; column += pixel.w) {
             pixel.x = column;
             pixel.y = row;
-            int pos = column/10 + row/10 * 64;
+            int pos = column/pixel.w + row/pixel.h * c8_width;
             
-            if (screen[pos] == 1) {
+            if (screen[pos] == 1) { // pixel is drawn (white)
                 SDL_FillRect(base_surface, &pixel, SDL_MapRGB(base_surface->format, 0xFF, 0xFF, 0xFF));
             }
-            else
+            else    // pixel is erased/not drawn (black)
             {
                 SDL_FillRect(base_surface, &pixel, SDL_MapRGB(base_surface->format, 0x00, 0x00, 0x00));
             }
@@ -61,19 +84,16 @@ void draw_graphics(chip8& game, SDL_Window*& window, SDL_Surface*& base_surface,
 }
 
 void set_keys(chip8& game, SDL_Event& event) {
+    /*
+     * Changes keyboard state according to currently pressed keys 
+     */
+
     // set key state to 1 if pressed
     if (event.type == SDL_KEYDOWN) {
         switch (event.key.keysym.sym) {
             case SDLK_1: 
                 game.keyboard[KEY_PRESS_1] = 1;
-
-                std::cout << "" << std::endl; 
-                for (auto key : game.keyboard) {
-                    std::cout << (int) key << std::endl;
-                }
-                std::cout << "" << std::endl;
-
-            break;
+                break;
                  
             case SDLK_2: 
                 game.keyboard[KEY_PRESS_2] = 1;
@@ -143,7 +163,7 @@ void set_keys(chip8& game, SDL_Event& event) {
         switch (event.key.keysym.sym) {
             case SDLK_1: 
                 game.keyboard[KEY_PRESS_1] = 0; 
-            break;
+                break;
                  
             case SDLK_2: 
                 game.keyboard[KEY_PRESS_2] = 0;
@@ -208,44 +228,63 @@ void set_keys(chip8& game, SDL_Event& event) {
     }
 }
 
-int main(int argc, const char* argv[]) {
-    chip8 game;
-    game.load_rom(argv[1]);
-
-    const int SCREEN_WIDTH = 640;
-    const int SCREEN_HEIGHT = 320;
-
-    SDL_Window* main_window = NULL;
-    SDL_Surface* base_surface = NULL;
+void game_loop(chip8& game, SDL_Window*& window, SDL_Surface*& base_surface, Mix_Chunk*& beep) {
+    SDL_Event event;
     
-    // SDL_Event event;
-    bool exit_flag = 0;
-
-    init_window(main_window, base_surface, SCREEN_WIDTH, SCREEN_HEIGHT);
-    SDL_UpdateWindowSurface(main_window);
-    
-        while (true) {
+    while (true) {
             game.emulate_cycle();
-            
-            SDL_Event event;
-            
-            while(SDL_PollEvent(&event)) {
+        
+            while(SDL_PollEvent(&event)) {  // set key actions
                 set_keys(game, event);
-                
+
                 if (event.type == SDL_QUIT) {
                 exit(0);
                 }
             }
             
             if (game.draw_flag) {
-                draw_graphics(game, main_window, base_surface, SCREEN_WIDTH, SCREEN_HEIGHT);
+                draw_graphics(game, window, base_surface);
+                game.draw_flag = false;
             }
-            game.draw_flag = 0;
 
-            std::this_thread::sleep_for(std::chrono::microseconds(1200)); 
-        
+            if (game.sound_timer > 0) {
+                Mix_PlayChannel(-1, beep, 0);
+            }
+
+            std::this_thread::sleep_for(std::chrono::microseconds(1000)); 
         }
+}
 
-    quit(main_window);
+int main(int argc, const char* argv[]) {
+    init_sdl();
+    chip8 game;
+
+    // check for number of arguments and load ROM
+    if (argc == 2) {
+        const char* rom = argv[1];
+        if(!game.load_rom(rom)) {
+            std::cout << "ROM not loaded" << std::endl;
+            exit(0);
+        }
+    }
+    else {  // incorrect number of arguments
+        std::cout << "Usage: ./chip-oct rom_name" << std::endl;
+        exit(0);
+    }
+    
+    // data of display
+    const int SCREEN_WIDTH = 960;
+    const int SCREEN_HEIGHT = 480;
+    SDL_Window* main_window = NULL;
+    SDL_Surface* base_surface = NULL;
+    
+    // initialize window and beep object
+    init_window(main_window, base_surface, SCREEN_WIDTH, SCREEN_HEIGHT);
+    Mix_Chunk* beep = Mix_LoadWAV("resources/beep.wav");    // for sound timer
+    
+    // begin game loop
+    game_loop(game, main_window, base_surface, beep);
+    
+    exit(0);
     return 0;
 }
